a580542eed42b28f02f837ea69a2e2e922ffc91c
Damian <damian_joniec@interia.pl>
2019-09-17 01:02:27 +0200

    new button added

--- js/scriptjs.js
+++ js/scriptjs.js
@@ -6,3 +6,9 @@ function myFunction() {
   x.innerHTML= "Rakieta FALCON wystartowała";
 }
 
+function landRocket(){
+	var x = document.getElementById("demo");
+	x.style.fontsize = "20";
+	x.style.color = "green";
+	x.innerHTML = "Rakieta Wylądowała";
+}
