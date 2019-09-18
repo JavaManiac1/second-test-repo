6f5f77e861a6b4b0de4996dc17a7be5cfea7911d
Damian <damian_joniec@interia.pl>
2019-09-18 13:26:21 +0200

    script modified

--- js/scriptjs.js
+++ js/scriptjs.js
@@ -12,3 +12,25 @@ function landRocket(){
 	x.style.color = "green";
 	x.innerHTML = "Rakieta Wylądowała";
 }
+
+function addBorder(){
+	var container = document.getElementsByClassName('container');
+    var divs = container[0].getElementsByTagName('div');
+	for (var i = 0; i < divs.length; i++) {
+		divs[i].style.border = "thin dotted green";
+	}
+    
+}
+
+	
+
+/*function addBorder(){
+	var nodes = document.getElementById('container').childNodes;
+	  for var(i=0; i<nodes.length; i++){
+         nodes[i].style.color = "red";
+         nodes[i].style.border = "dotted red";
+	  }
+  /* elemw =  document.getElementsByClassName("container");
+	elemw.innerHTML = "działa";
+	elemw.style.border = "dotted red";
+	elemw.style.backgroundColor = "red" */ 
